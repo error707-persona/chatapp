6d2ff096fd4f2b320fb6deabc263721df796db3b
error707-persona <areeshasayed09@gmail.com>
2023-03-11 20:13:32 +0530

    added delete button

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -5,7 +5,9 @@ import { collection, addDoc } from "firebase/firestore";
 import { auth } from "../firebase";
 import send from "../assests/send.svg";
 import attach from "../assests/attach.svg";
+import emoji from "../assests/emoji.svg";
 import AppContext from "../context/AppContext";
+import EmojiPicker from "emoji-picker-react";
 import { app, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
@@ -15,6 +17,8 @@ const InputBox = () => {
   const table = context.table;
   const collectionsRef = collection(database, table);
   const [file, setfile] = useState("");
+  const [emojiPicker, setemojiPicker] = useState(false);
+  const [imagePicker, setimagePicker] = useState(false);
   const storageRef = ref(storage, `images/${file.name}`);
   const handleSend = () => {
     if (data !== "") {
@@ -58,6 +62,25 @@ const InputBox = () => {
       <button onClick={handleSend} className="send">
         <img src={send} />
       </button>
+      <div className="dropdown">
+        <div className="dropdown-menu" style={{ opacity: emojiPicker ? 1 : 0 }}>
+          <EmojiPicker />
+        </div>
+        <button className="send" onClick={() => setemojiPicker(!emojiPicker)}>
+          <img src={emoji} />
+        </button>
+      </div>
+      <div className="dropdown">
+        <div className="dropdown-menu"  style={{ opacity: imagePicker ? 1 : 0 }}>
+          <input type="file"/>
+        
+          <button onClick={handleSubmit} className="submit">Send</button>
+        </div>
+          <button className="send" onClick={() => setimagePicker(!imagePicker)}>
+            <img src={attach} />
+          </button>
+        
+      </div>
     </div>
   );
 };
--- src/components/Message.js
+++ src/components/Message.js
@@ -6,6 +6,7 @@ import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import AppContext from "../context/AppContext";
+import trash from "../assests/trash.svg"
 const Message = () => {
   const dummy = useRef();
   const context = useContext(AppContext);
@@ -13,10 +14,15 @@ const Message = () => {
   const setdata = context.setdata;
   const table = context.table;
   data.sort((a, b) => a.timestamp - b.timestamp);
+
+  const handleDeleteChat = () => {
+    
+  }
   return (
     <div className="main">
       <div className="currentuser">
         <div className="partner">
+          
           <div className="avatar">
             <img
               src={profile}
@@ -26,13 +32,18 @@ const Message = () => {
             />
           </div>
           {(auth.currentUser!==null)?(auth.currentUser.email===table.slice(9).split(",")[0])?table.slice(9).split(",")[1]:table.slice(9).split(",")[0]:table.slice(9).split(",")[1]}
+          <div className="deleteChat">
+            <button style={{borderRadius:"50%", border:0, width:"50px",height:"50px"}} onClick={handleDeleteChat}> 
+              <img src={trash} width={30} height={30}/>
+            </button>
+          </div>
         </div>
         {data?.map((item) =>
           item.username === auth.currentUser.email ? (
             <div className="block-user">
-              <div>
+              <div style={{display:"flex", flexDirection:"column"}}>
                 <span className="username">{item.username}</span>
-                <div className="usermsg">{item.msg}</div>
+                <div className="usermsg" style={{marginLeft:"auto"}}>{item.msg}</div>
                 <br></br>
                 {/* <div>{Date(item.timestamp).split(" ").slice(4, 5)}</div> */}
               </div>

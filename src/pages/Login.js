0fed04744c2f71c8650f5d9e5c49527e8d35866f
Sayed Areesha <areeshasayed09@gmail.com>
2023-04-15 00:03:51 +0530

    fixed routes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,17 +2,22 @@ import React from "react";
 import { auth } from "../firebase";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import googlelogo from "../assests/google.avif";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const navigate = useNavigate();
   
   let googleProvider = new GoogleAuthProvider();
   const handleSignIn = () => {
     // AUTH ****************
     signInWithPopup(auth, googleProvider)
       .then((res) => {
-        window.location.href = "/chatapp";
+        // window.location.href = "/chatapp";
+        navigate("/chatapp");
       })
       .catch((err) => {
-        window.location.href = "/";
+        // window.location.href = "/";
+        navigate("/");
       });
   };
   return (
